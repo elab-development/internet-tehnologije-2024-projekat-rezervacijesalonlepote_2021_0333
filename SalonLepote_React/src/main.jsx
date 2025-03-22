266d2ae9193ee9992e41f8984c48e987274dcea0
jovanapetrovic15 <jovanapb02@gmail.com>
2025-03-22 11:53:24 +0100

    Kreiranje osnovnih komponenti

--- SalonLepote_React/src/main.jsx
+++ SalonLepote_React/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {ContextProvider} from './context/ContextProvider.jsx'
+import {RouterProvider} from 'react-router-dom'
+import router from './router/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+    <ContextProvider>
+      <RouterProvider router={router}></RouterProvider>
+
+    </ContextProvider>
+ 
 )
